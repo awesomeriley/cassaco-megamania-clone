ff9dd3daa26ff2ae8f0eb8dbf2b7e3814df4038c
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-01 13:41:12 +0000

    acrescimo da classe responsavel pelo tratamento de fontes ttf

---
+++ Project/Megamania/sdl_font.cpp
@@ -0,0 +1,107 @@
+/**********************************************************************
+ * Classe responsavel por manipular os dados relacionados com 
+ * os sons do jogo , bem como a musica.
+ * Esta classe possui operações básicas com tocar um som, uma musica
+ * realizar operações de FADE, aumentar e diminuir o volume do som
+ * ou música e pausar.
+ *
+ * Author: Adriano Braga Alencar (adrianobragaalencar@gmail.com) 
+ *
+ *********************************************************************/
+#include "sdl_font.h"
+#include <string.h>
+
+namespace Megamania
+{
+
+	/******************************************************************
+	 * Construtor padrão responsavel por criar uma nova Font carregando
+	 * os dados do arquivo .ttf informado
+	 *
+	 * file -> caminho para o arquivo .ttf
+	 * size -> indica o tamanho da fonte
+	 *
+	 ******************************************************************/
+	SDL_Font::SDL_Font(const char *file, int size)throw(SDLFontException)
+	{
+		if(file == NULL) {
+			throw SDLFontException("O objeto Font não pode ser criado, [PATH NULL]");
+		}
+		if((font = TTF_OpenFont(file, size)) == NULL) {
+			throw SDLFontException("O objeto Font não pode ser criado");
+		}
+		color.r = color.g = color.b = 0xff;    //branco
+	}
+
+	/******************************************************************
+	 * Destruidor responsavel por desalocar os recursos alocados pelo 
+	 * sistema TTF
+	 *
+	 ******************************************************************/
+	SDL_Font::~SDL_Font(void)
+	{
+		TTF_CloseFont(font);
+	}
+
+	/******************************************************************
+	 * Função responsavel por retornar a surface com o texto informado
+	 *
+	 * text -> indica o texto a ser renderizado na superficie, caso
+	 * seja passado como parametro um valor NULL será retornado uma
+	 * superficie NULL
+	 * SDL_Surface -> superficie retornada com o texto, ou null caso 
+	 * ocorra algum erro
+	 *
+	 ******************************************************************/
+	SDL_Surface * SDL_Font::RenderTextSolid(const char *text) 
+	{
+		if((text != NULL)&&(strlen(text))) {
+			return TTF_RenderText_Solid(font, text, color);
+		}
+		return NULL;
+	}
+
+	/******************************************************************
+	 * Função responsavel por retornar o valor indicando a altura da
+	 * font
+	 *
+	 * int -> indica a altura da fonte em pixels
+	 *
+	 ******************************************************************/
+	const int SDL_Font::GetHeight()const
+	{
+		return TTF_FontHeight(font);
+	}
+
+	/******************************************************************
+	 * Função responsavel por setar a cor responsavel por pintar
+	 * a font
+	 *
+	 * r -> indica o tom vermelho da cor
+	 * g -> indica o tom verde da cor 
+	 * b -> indica o tom azul da cor
+	 *
+	 ******************************************************************/
+	void SDL_Font::SetColor(Uint8 r, Uint8 g, Uint8 b) 
+	{
+		color.r = r;
+		color.g = g;
+		color.b = b;
+	}
+
+	/******************************************************************
+	 * Função responsavel por retornar o valor da cor atual aplicada
+	 * a font
+	 *
+	 * r -> variavel que será preenchida com o tom vermelho
+	 * g -> variavel que será preenchida com o tom verde
+	 * b -> variavel que será preenchida com o tom azul
+	 *
+	 ******************************************************************/
+	void SDL_Font::GetColor(Uint8 &r, Uint8 &g, Uint8 &b)
+	{
+		r = color.r;
+		g = color.g;
+		b = color.b;
+	}
+}
