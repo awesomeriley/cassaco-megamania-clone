14156910f55909ba60f36f2c963b9ece2d2237f6
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-01 13:48:35 +0000

    acrescimo da classe que controla todo o fluxo do jogo

---
+++ Project/Megamania/game_controller.cpp
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Classe responsavel por controlar toda a lógica do JOGO, está classe
+ * é responsavel por inicializar todos os sistemas do SDL, capturar todos
+ * os eventos e controlar a logica do jogo, como atualização de tela,
+ * atualização de NPC etc.
+ *
+ *  Author Adriano Braga Alencar (adrianobragaalencar@gmail.com)
+ *
+ *******************************************************************************/
+#include "game_controller.h"
+#include "initializer.h"
+
+namespace Megamania 
+{
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::OnGameInit(void) 
+	{
+		Initializer::Init();
+	}
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::OnGameEvent(SDL_Event *event)
+	{			
+		switch(event->type) {
+			case SDL_USEREVENT:
+				switch(event->user.code) {
+					case SPLASH_SCREEN_ID_EVENT:
+						InitMenuScreen();
+						break;
+				}
+		}		
+	}
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::OnGameStop(void)
+	{
+		running = false;
+	}
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::OnGameExit(void)
+	{
+		Initializer::Shutdow();
+	}
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::OnGameDraw(void)
+	{
+	}
+	
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::InitSpashScreen(void) 
+	{
+		splashScreen->Show();		
+	}
+
+	/*******************************************************************************
+	 * 
+	 *******************************************************************************/
+	void GameController::InitMenuScreen(void) 
+	{
+		menuScreen->Show();
+	}
+}
