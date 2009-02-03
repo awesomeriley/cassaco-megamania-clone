f8df8c8891b7ba787ccb0fb4659531ac88d82e46
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-03 12:55:34 +0000

    acrescimo de novos comentaios

--- Project/Megamania/abstract_screen.cpp
+++ Project/Megamania/abstract_screen.cpp
@@ -1,15 +1,20 @@
-/******************************************************************
+/********************************************************************
  * Classe que representa a classe base para todas as classes do
  * jogo, esta classe apresenta os seguintes estados que deverão
  * ser implementadas por todas as sub-classes:
  *
- *
- *
- *
+ * -OnInit -> Função responsavel por iniciar o estado da tela
+ * -OnExecute -> Função responsavel por exibir a tela 
+ * -OnEvent -> Função de callback que será chamada pelo gerenciador
+ * de eventos quando ocorrer algum evento, repassando assim para a 
+ * tela o evento que ocorreu
+ * -OnDraw -> Função responsavel por pintar ou repintar a tela
+ * -OnClear -> Função que deve ser chamada quando a tela não for
+ * mais necessaria, realizando assim uma limpeza antes de sair
  *
  * Author: Adriano Braga Alencar (adrianobragaalencar@gmail.com) 
  * 
- *****************************************************************/
+ *******************************************************************/
 #include "abstract_screen.h"
 
 namespace Megamania
