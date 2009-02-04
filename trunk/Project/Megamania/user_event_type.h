0e9b65d823222777468abdc0b1c38f0e6f9bbd54
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-04 01:08:57 +0000

    acrescimo da classe que contem as constantes de eventos

---
+++ trunk/Project/Megamania/user_event_type.h
@@ -0,0 +1,17 @@
+/********************************************************************
+ * Arquivo que contém todas os tipos de eventos definidos para o 
+ * jogo, aqui são definidos os eventos que podem ocorrer durante a 
+ * execução do Game
+ *
+ *  Author Adriano Braga Alencar (adrianobragaalencar@gmail.com)
+ *
+ ********************************************************************/
+#ifndef __USER_EVENT_TYPE_H__
+#define __USER_EVENT_TYPE_H__
+
+#include "SDL.h"
+
+const static Uint8 SPLASH_SCREEN_ID_EVENT = 0x00000001;
+const static Uint8 MENU_SCREEN_ID_EVENT = 0x00000002;
+
+#endif //__USER_EVENT_TYPE_H__
