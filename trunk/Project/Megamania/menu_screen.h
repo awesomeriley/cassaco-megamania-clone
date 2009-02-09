0e89747d3eb81a38c7b6b2cda73c7937c6b88423
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-09 17:29:34 +0000

    acrescimo de uma classe listener

--- trunk/Project/Megamania/menu_screen.h
+++ trunk/Project/Megamania/menu_screen.h
@@ -13,10 +13,11 @@
 
 #include "SDL.h"
 #include "cs_button.h"
-#include "sdl_font.h"
 #include "game_config.h"
 #include "abstract_screen.h"
 #include "video_exception.h"
+#include "menu_listener.h"
+#include <vector>
 
 namespace Megamania
 {
@@ -25,6 +26,8 @@ namespace Megamania
 		private:
 			/** surface que indica o background do JOGO*/
 			SDL_Surface *background;
+			/** lista de todos os objetos interessados em ouvir os eventos*/
+			std::vector<MenuScreenListener *> listeners;
 			/** Botão responsavel por iniciar um novo JOGO*/
 			CSButton *startBT;
 			/** Botão responsavel por abrir a tela de opções*/
@@ -36,6 +39,8 @@ namespace Megamania
 		public:
 			MenuScreen(SDL_Surface *)throw(SDLVideoException);
 			~MenuScreen(void);
+			void AddListener(MenuScreenListener *);
+			void RemoveListener(MenuScreenListener *);
 			void Init(void);
 			void Execute(void);
 			void Event(SDL_Event *);
