12ae67dddf28435e77b4cb3c24c2346bea6f5ce5
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-11 11:32:52 +0000

    modificação realizada no movimento da bala

--- trunk/Project/Megamania/bullet.cpp
+++ trunk/Project/Megamania/bullet.cpp
@@ -12,7 +12,8 @@ namespace Megamania
 {
 
 	/*************************************************************
-	 *
+	 * Construtor padrão que cria um novo objeto Bullet
+	 * 
 	 *************************************************************/
 	Bullet::Bullet(void)throw(SpriteException) :
 	GameObject(PATH_BULLET, BULLET_WIDTH, BULLET_HEIGHT)
@@ -20,6 +21,8 @@ namespace Megamania
 	}
 
 	/*************************************************************
+	 * Destruidor responsavel por desalocar todos os recursos
+	 * alocados pelo Objeto Bullet
 	 *
 	 *************************************************************/
 	Bullet::~Bullet(void) 
@@ -27,18 +30,21 @@ namespace Megamania
 	}
 
 	/*************************************************************
+	 * Função que atualiza a posição atual da bala, esta função
+	 * recebe como parametro um valor indicando se a bala deverá
+	 * subir ou deverá descer
 	 *
 	 *************************************************************/
 	void Bullet::Update(Direction direction) 
 	{
 		if(direction == Direction::UP) {
-			if(GetY() - BULLET_OFFSET > 0) {
+			if(GetY() - BULLET_OFFSET > - GetHeightFrame()) {
 				Move(0, -BULLET_OFFSET);
 			} else {
 				SetVisible(false);
 			}			
 		} else {
-			if(GetY() + BULLET_OFFSET < HEIGHT_SCREEN) {
+			if(GetY() + BULLET_OFFSET < HEIGHT_SCREEN + GetHeightFrame()) {
 				Move(0, BULLET_OFFSET);
 			} else {
 				SetVisible(false);
