5d89429cbd9a49db0a8fa770f2eb9dc54fe5529e
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-02-12 02:56:34 +0000

    acrescimo da super classe

---
+++ Project/Megamania/sdl_sound.h
@@ -0,0 +1,27 @@
+/********************************************************************
+ * Classe que representa a abstração para um som qualquer, podendo 
+ * representar um efeito sonoro, bem como uma musica a ser tocada em
+ * um canal exclusivo
+ *
+ * Author: Adriano Braga Alencar (adrianobragaalencar@gmail.com) 
+ *
+ ********************************************************************/
+#ifndef __SDL_SOUND_H__
+#define __SDL_SOUND_H__
+
+#include "audio_exception.h"
+
+namespace Megamania
+{
+	class Sound
+	{
+		public:
+			virtual ~Sound(void) {};
+			virtual void Load(const char *)throw(SDLAudioException) = 0;
+			virtual void SetVolume(int volume = MIX_MAX_VOLUME) = 0;
+			virtual void Play(void) = 0;
+			virtual void Close(void) = 0;
+	};
+}
+
+#endif //__SDL_SOUND_H__
