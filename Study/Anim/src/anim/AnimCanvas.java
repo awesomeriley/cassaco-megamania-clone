e4b2cee416650d9d26661fe44dc948c632f89f03
janebro <janebro@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-03-06 14:13:52 +0000

    Movimenação inicial do sprite.

--- Study/Anim/src/anim/AnimCanvas.java
+++ Study/Anim/src/anim/AnimCanvas.java
@@ -7,26 +7,30 @@ import javax.microedition.lcdui.game.Sprite;
 
 public class AnimCanvas extends GameCanvas implements Runnable{
   
+  private final int girlXVel = 4;
   private int[] WALK = {1, 2, 3, 4, 5, 6, 7, 8};
   private int[] STAND = {0};
   private int sleepTime = 60;
+  private int girlY = 0;
+  private int girlX = 0;
   private boolean isWalking = false;
   private Image girl;
   private Sprite girlSprite;
-  
 
-  protected AnimCanvas() {
+
+  public AnimCanvas() {
     super(false);
   }
-  
+
   public void start() {
     try {
       girl = Image.createImage("/girl_sprites.png");
       girlSprite = new Sprite(girl, 64, 64);
+      girlSprite.setRefPixelPosition(girlSprite.getWidth()/2, girlSprite.getHeight()/2);
     } catch (Exception e) {
       e.printStackTrace();
     }
-    
+
     Thread thread = new Thread(this);
     thread.start();
   }
@@ -41,23 +45,56 @@ public class AnimCanvas extends GameCanvas implements Runnable{
       }
     }
   }
-  
+
   private void createBackground(Graphics g) {
     g.setColor(192, 192, 192);
     g.fillRect(0, 0, getWidth(), getHeight());
   }
-  
+
   private void updateScreen(Graphics g) {
     createBackground(g);
-    girlSprite.setPosition(getWidth()/2 - girlSprite.getWidth()/2, getHeight()/2 - girlSprite.getHeight()/2);
+    girlSprite.setPosition(girlX, girlY);
     
-    if (!isWalking) {
-      girlSprite.setFrameSequence(this.WALK);
-      isWalking = true;
+    if (getKeyStates() != 0) {
+      moveGirl(g);
     } else {
-      girlSprite.nextFrame();
-      girlSprite.paint(g);
-      flushGraphics();
+      standGirl(g);
     }
+    flushGraphics();
+  }
+
+  private void moveGirl(Graphics g) {
+    int keyState = getKeyStates();
+    
+    if ((keyState & RIGHT_PRESSED) != 0 && girlX < (getWidth() - girlSprite.getWidth())) {
+      girlSprite.setTransform(Sprite.TRANS_NONE);
+      
+      if (!isWalking) {
+        isWalking = true;
+        girlSprite.setFrameSequence(this.WALK);
+      } else {
+        girlSprite.nextFrame();
+        girlSprite.paint(g);
+      }
+      girlX += girlXVel;
+    } else if ((keyState & LEFT_PRESSED) != 0 && girlX > 0) {
+      girlSprite.setTransform(Sprite.TRANS_MIRROR);
+      
+      if (!isWalking) {
+        isWalking = true;
+        girlSprite.setFrameSequence(this.WALK);
+      } else {
+        girlSprite.nextFrame();
+        girlSprite.paint(g);
+      }
+      girlX -= girlXVel;
+    }
+  }
+  
+  private void standGirl(Graphics g) {
+    isWalking = false;
+    girlSprite.setFrameSequence(this.STAND);
+    girlSprite.nextFrame();
+    girlSprite.paint(g);
   }
 }
