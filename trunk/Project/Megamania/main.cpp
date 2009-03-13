9cfc9d73f298cde6e5c31d0c1c96e8dd35517850
adrianobragaalencar <adrianobragaalencar@a610f558-e72f-11dd-8fb1-9960f2a117f8>
2009-03-13 01:31:29 +0000

    sobrecarga dos operadores new e delete para que se possa debugar o gerenciamento de memoria

--- trunk/Project/Megamania/main.cpp
+++ trunk/Project/Megamania/main.cpp
@@ -5,9 +5,48 @@
 #include <cstdlib>
 #include "game_controller.h"
 
-#define LOG
-
 using namespace Megamania;
+
+#ifdef DEBUG_MEMORY    //descomente essa linha para debugar o gerenciamento de memoria
+
+#include "log.h"
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_SIZE 5
+
+static int new_c = 1;
+static int del_c = 1;
+static char buffer[BUF_SIZE];
+static char buffer0[BUF_SIZE*5];
+
+#ifdef delete
+#undef delete
+#endif
+
+inline void * operator new(size_t size) {
+
+	memset(buffer, 0, BUF_SIZE);
+	memset(buffer0, 0, BUF_SIZE*5);
+	sprintf(buffer, "%d", new_c);
+	sprintf(buffer0, "%s%s%s", "Operador NEW chamado ", buffer, " vezes");
+	LOG_INFO(buffer0);
+	new_c++;
+	return (void *)malloc(size);
+}
+
+inline void operator delete(void *p) {
+
+	memset(buffer, 0, BUF_SIZE);
+	memset(buffer0, 0, BUF_SIZE*5);
+	sprintf(buffer, "%d", del_c);
+	sprintf(buffer0, "%s%s%s", "Operador DELETE chamado ", buffer, " vezes");
+	LOG_INFO(buffer0);
+	del_c++;
+	free(p);
+}
+
+#endif
 
 int main(int argc, char **argv) 
 {	
